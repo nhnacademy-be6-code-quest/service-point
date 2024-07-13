0bf49e9450f5e39139c240f0ad9a64ddb1361644
jjeonmin <jms959310@gmail.com>
2024-07-13 20:22:36 +0900

    feat: rabbitmq 수정

--- src/main/java/com/service/point/config/ReviewMessageDto.java
+++ src/main/java/com/service/point/config/ReviewMessageDto.java
@@ -2,9 +2,11 @@ package com.service.point.config;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class ReviewMessageDto {
     Long clientId;
     Boolean hasImage;
