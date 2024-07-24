7835072e78ad71cfc4c6cb5235136c5efbe20984
jjeonmin <jms959310@gmail.com>
2024-07-24 22:47:15 +0900

    test: 테스트 코드 추가

--- src/test/java/com/service/point/config/RabbitConfigTest.java
+++ src/test/java/com/service/point/config/RabbitConfigTest.java
@@ -12,8 +12,10 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
  class RabbitConfigTest {
 
     @Autowired
